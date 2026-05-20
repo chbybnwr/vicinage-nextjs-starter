e8f08cff5967ef9f28ec82455c4b06e88d14f788
Chabib N. <chabib.nr@gmail.com>
2026-05-20 08:51:38 +0700

    chore: add stylex plugin to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
 import vicinage from '@vicinage/eslint-plugin'
+import stylex from '@stylexjs/eslint-plugin'
 import prettierConfig from 'eslint-config-prettier/flat'
 
 const eslintConfig = defineConfig([
@@ -17,6 +18,24 @@ const eslintConfig = defineConfig([
   ]),
 
   vicinage.configs.recommended,
+
+  {
+    plugins: {
+      '@stylexjs': stylex,
+    },
+    rules: {
+      '@stylexjs/no-conflicting-props': 'error',
+      '@stylexjs/no-nonstandard-styles': 'error',
+      '@stylexjs/no-legacy-contextual-styles': 'error',
+      '@stylexjs/valid-styles': 'error',
+      '@stylexjs/valid-shorthands': 'warn',
+      '@stylexjs/sort-keys': 'warn',
+      '@stylexjs/no-lookahead-selectors': 'warn',
+      '@stylexjs/enforce-extension': 'warn',
+      '@stylexjs/no-unused': 'off',
+    },
+  },
+
   prettierConfig,
 ])
 
